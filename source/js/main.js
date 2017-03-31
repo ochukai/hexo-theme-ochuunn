f33425a7b3643e1cd880ccb2a2f240d88cd653da
ochukai <sex.xiaoyee@gmail.com>
2017-03-31 17:35:17 +0800

    add theme file

---
+++ source/js/main.js
@@ -0,0 +1,145 @@
+$(function () {
+	// console.log('I am Oliver~');
+
+	// image preview
+	$('.post__main img').each(function (index, item) {
+		var $img = $(this);
+		var className = 'modalBox' + index;
+
+		var $tpl = $('<div />');
+		$tpl.addClass('modalBox').addClass(className).append($($img.prop('outerHTML')));
+		$tpl.hide();
+
+		$img.on('click', function () {
+			$tpl.modalBox({
+				onOpen: function () {
+					$('body').addClass('modal-open');
+				},
+				onClose: function () {
+					$('body').removeClass('modal-open');
+				}
+			});
+		});
+
+		$('body').append($tpl);
+	});
+
+	// key bind
+
+	// j  down
+	// k  top
+	// t  page top
+	// b  page bottom
+
+	// i  go index 
+	var $body = $('html');
+
+	var isKeydown = false;
+	$body.on('keydown', function (e) {
+		// console.log(e.which, 'key down');
+
+		switch (e.which) {
+			case 74: // j down
+				if (!isKeydown) {
+					isKeydown = true;
+					requestAnimationFrame(function animate() {
+						var curTop = window.scrollY;
+						window.scrollTo(0, curTop + 15);
+
+						if (isKeydown) {
+							requestAnimationFrame(animate);
+						}
+					});
+				}
+
+				break;
+
+			case 75: // k up
+				if (!isKeydown) {
+					isKeydown = true;
+					requestAnimationFrame(function animate() {
+						var curTop = window.scrollY;
+						window.scrollTo(0, curTop - 15);
+
+						if (isKeydown) {
+							requestAnimationFrame(animate);
+						}
+					});
+				}
+
+				break;
+
+			case 191: // shift + / = ? show help modal 
+				break;
+
+				// 16 shift
+			case 84: // t
+				window.scrollToTop(1);
+				break;
+
+			case 66: // b
+				window.scrollToBottom();
+				break;
+
+			case 78: // n half
+				window.scrollPageDown(1);
+				break;
+
+			case 77: // m
+				window.scrollPageUp(1);
+				break;
+		}
+
+	});
+
+	$body.on('keyup', function (e) {
+		isKeydown = false;
+	});
+
+	// print hint
+
+	var comments = [
+		'',
+		'                    .::::.            快捷键：',
+		'                  .::::::::.            j：下移',
+		'                 :::::::::::            k：上移',
+		"             ..:::::::::::'             t：移到最顶",
+		"           '::::::::::::'               b：移到最底",
+		'             .::::::::::                n：下移很多',
+		"        '::::::::::::::..               m：上移很多",
+		'             ..::::::::::::.',
+		'           ``::::::::::::::::',
+		"            ::::``:::::::::'        .:::.",
+		"           ::::'   ':::::'       .::::::::.",
+		"         .::::'      ::::     .:::::::'::::.",
+		"        .:::'       :::::  .::::::::'  ':::::.",
+		"       .::'        :::::::::::::::'      ':::::.",
+		"      .::'        :::::::::::::::'          ':::.",
+		"  ...:::          :::::::::::::'              ``::.",
+		" ```` ':.         '::::::::::'                  ::::..",
+		"                    ':::::'                    ':'````..",
+		''
+	];
+
+	comments.forEach(function (item) {
+		console.log('%c' + item, 'color: #399c9c');
+	});
+
+	$('.btn-reward').on('click', function (e) {
+		e.preventDefault();
+
+		var $reward = $('.reward-wrapper');
+		$reward.slideToggle();
+	});
+
+	$('header.head').addClass('queue-in');
+	$('article.post').each(function (index) {
+		var $this = $(this);
+		$this.css({
+			'animationDelay': .1 * (index + 1) + 's'
+		});
+
+		$this.addClass('queue-in');
+	});
+
+});
---
+++ source/js/particles.oli.js
@@ -0,0 +1,127 @@
+$(function () {
+
+    particlesJS("particles-oli-wrapper", {
+        "particles": {
+            "number": {
+                "value": 40,
+                "density": {
+                    "enable": true,
+                    "value_area": 800
+                }
+            },
+            "color": {
+                "value": "#399c9c"
+            },
+            "shape": {
+                "type": "circle",
+                "stroke": {
+                    "width": 0,
+                    "color": "#000000"
+                },
+                "polygon": {
+                    "nb_sides": 5
+                },
+                // "image": {
+                //     "src": "img/github.svg",
+                //     "width": 100,
+                //     "height": 100
+                // }
+            },
+            "opacity": {
+                "value": 0.4,
+                "random": false,
+                "anim": {
+                    "enable": false,
+                    "speed": 1,
+                    "opacity_min": 0.2,
+                    "sync": false
+                }
+            },
+            "size": {
+                "value": 20,
+                "random": true,
+                "anim": {
+                    "enable": false,
+                    "speed": 30,
+                    "size_min": 0.1,
+                    "sync": false
+                }
+            },
+            "line_linked": {
+                "enable": true,
+                "distance": 250,
+                "color": "#40afaf",
+                "opacity": 0.5,
+                "width": 1
+            },
+            "move": {
+                "enable": true,
+                "speed": 3,
+                "direction": "none",
+                "random": true,
+                "straight": false,
+                "out_mode": "out",
+                "bounce": true,
+                "attract": {
+                    "enable": false,
+                    "rotateX": 600,
+                    "rotateY": 1200
+                }
+            }
+        },
+        "interactivity": {
+            "detect_on": "canvas",
+            "events": {
+                "onhover": {
+                    "enable": true,
+                    "mode": "grab"
+                },
+                "onclick": {
+                    "enable": true,
+                    "mode": "push"
+                },
+                "resize": true
+            },
+            "modes": {
+                "grab": {
+                    "distance": 140,
+                    "line_linked": {
+                        "opacity": 1
+                    }
+                },
+
+            }
+        },
+        "retina_detect": true
+    });
+
+    var $particle = $('#particles-oli-wrapper').css({ display: 'none'}).removeClass('hide');
+    var $navH1 = $('.index-wrapper header h1, .index-wrapper header nav').hide();
+
+    var $header = $('.index-wrapper .hide')
+        .removeClass('hide')
+        .css({
+            height: 0
+        })
+        .animate({
+            height: 120
+        }, 800, function() {
+            $particle.fadeIn(600);
+            $navH1.fadeIn(600);
+        });
+
+
+    $('.head-nav__link').on('click', function(e) {
+        e.preventDefault();
+        
+        $particle.fadeOut(600);
+        $navH1.fadeOut(600);
+
+        $header.animate({
+            height: 0
+        }, 800, function() {
+            window.location.href = '/archives';
+        });
+    });
+
+});
